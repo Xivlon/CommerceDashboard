6ee90b25626bfb3a46c18ea397938cbf70a9b99e
Nuruddin <104330069+Xivlon@users.noreply.github.com>
2025-08-18 12:21:29 +0000

    Trial 1

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
